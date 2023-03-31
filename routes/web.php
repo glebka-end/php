977b2aa3115c9d171400b7b8a1d78cf79829e9a0
“glebka-end” <pytko.gleba@gmail.com>
2023-03-31 14:09:20 +0300

    тест_добавил в web.php rote::post and created PostCotroller.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,17 @@ Route::get('/', function () {
 Route::get('/e', function () {
     return 'welcome';
 });
+
+
+// CRUD (create, read, update, delete)
+//Route::get('posts',[PostController::class, 'index'])->name('posts');
+//Route::get('posts/create',[PostController::class, 'create'])->name('postes.create');
+//Route::post('posts',[PostController::class, 'store'])->name('posts.store');
+//Route::get('posts/{post}',[PostController::class ,'show'])->name('posts.show');
+//Route::get('posts/{post}/edit',[PostController::class, 'edit'])->name('posts.edit');
+//Route::put('posts/{post}',[PostController::class, 'update'])->name('posts.update');
+//Route::delete('posts/{post}',[PostController::class, 'delete'])->name('posts.delete');
+//like
+Route::resource('poste',PostController::class);
+Route::put('posts/{post}/like',[PostController::class, 'like'])->name('posts.like');
+// CRUD (create, read, update, delete)
