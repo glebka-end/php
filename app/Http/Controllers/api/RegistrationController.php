5d78cbd643085563ae6e8a2e9f2356aa49d6fdf9
“glebka-end” <pytko.gleba@gmail.com>
2023-04-03 17:28:44 +0300

    regis creat new user basic

---
+++ app/Http/Controllers/api/RegistrationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\api;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\RegistrationResourse;
+
+class RegistrationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return user::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $crated_desk =user::create($request->all());
+        return new RegistrationResourse($crated_desk);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\api\RegistrationController;//
 use App\Http\Controllers\PostControllerApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\UsersControllerApi;//
+use Doctrine\DBAL\Schema\Index;
+use phpseclib3\File\ASN1\Maps\Name;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,14 +24,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/test', function () {
-    return "wdedeedwdwwfwefwefwfwfww";
-    //return $request->user();
-});
-Route::apiresource('/poste',PostControllerApi::class);
+// Route::post('/test', function () {
+//     return "wdedeedwdwwfwefwefwfwfww";
+//     //return $request->user();
+// });
+// Route::apiresource('/poste',PostControllerApi::class);
 
-Route::post('/prim', 'MyPlaceController@index');
-Route::post('/primn', 'MyPlaceController@index2');
+// Route::post('/prim', 'MyPlaceController@index');
+// Route::post('/primn', 'MyPlaceController@index2');
 
-Route::get('/baisc', 'baisc@index');
+// Route::get('/baisc', 'baisc@index');
 
+
+// //Route::post('register', 'UserController@register');
+// Route::apiresource('/e',UsersControllerApi::class);
+
+Route::apiresource('/user_Registration',UsersControllerApi::class);
+Route::apiresource('/Registration',RegistrationController::class) ;
