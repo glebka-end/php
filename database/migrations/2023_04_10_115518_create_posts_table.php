4d1b547463e2af8205ba1e056235e3ed4d18b798
“glebka-end” <pytko.gleba@gmail.com>
2023-04-11 19:42:39 +0300

    create tabl commets and model comments

--- database/migrations/2023_04_10_115518_create_posts_table.php
+++ database/migrations/2023_04_10_115518_create_posts_table.php
@@ -12,16 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+         //   $table->foreign('id','post_comments_fk')->on('comments')->references('id_post');
+        //    $table->id('id_user');
+           
             $table->string('title');
             $table->text('contente');
             $table->string('image')->nullable();
             $table->unsignedBigInteger('likes')->nullable();
             $table->boolean('isPublished')->default(1);
             $table->timestamps();
+
+           // $table->unsignedBigInteger();
+           // $table->timestamps();
+           // $table->timestamps();
+
         });
     }
-
     /**
      * Reverse the migrations.
      */
